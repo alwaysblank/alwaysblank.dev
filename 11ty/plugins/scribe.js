af89f5c4f9c0d3f859d8017bba18546a541695c1
Ben <ben@alwaysblank.dev>
2022-04-09 13:06:32 -0700

    Add dl support to markdown parser

--- 11ty/plugins/scribe.js
+++ 11ty/plugins/scribe.js
@@ -1,7 +1,12 @@
 module.exports = conf => {
-    /**
-     * Scribe is loaded in its own file here to make it easier to
-     * customize its behavior (if you like).
-     */
-    conf.addPlugin(require('@11in/scribe'))
+  /**
+   * Scribe is loaded in its own file here to make it easier to
+   * customize its behavior (if you like).
+   */
+  conf.addPlugin(require('@11in/scribe'), {
+    markdown: engine => {
+      engine.use(require('markdown-it-deflist'))
+      return engine
+    }
+  })
 }
